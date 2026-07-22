2aefa51d862ba2df03f93a9b38bd4d53505e1adf
Mariano Popolo <75034036+popolo77@users.noreply.github.com>
2026-07-22 23:41:54 +0000

    Resuelvo ejercicio de Unidad

--- Bloque 1: Etapa Inicial e Introducción/ejercicio_introductorio.py
+++ Bloque 1: Etapa Inicial e Introducción/ejercicio_introductorio.py
@@ -1,1 +1,7 @@
+# =======================================================
+# Curso: Python Nivel 1 (Maxi Programa)
+# Unidad 1: Introducción
+# Ejercicio: Hola Mundo
+# =======================================================
+
 print("Hola Mundo!")
