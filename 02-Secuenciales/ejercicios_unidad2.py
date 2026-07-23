e9767ad96d061d5dad6badd8d73e5863e8c4c6ef
Mariano Popolo <75034036+popolo77@users.noreply.github.com>
2026-07-23 00:05:08 +0000

    Resuelvo ejercicio de Unidad

---
+++ 02-Secuenciales/ejercicios_unidad2.py
@@ -0,0 +1,13 @@
+# =======================================================
+# CLASE 3: Primer Programa (Suma de 2 números)
+# =======================================================
+# n1 = int(input("Ingrese primer número: "))
+# n2 = int(input("Ingrese segundo número: "))
+# print("Suma:", n1 + n2)
+
+
+
+# =======================================================
+# CLASE 4: [Nombre del siguiente tema]
+# =======================================================
+# Acá vas escribiendo el código del nuevo video...
